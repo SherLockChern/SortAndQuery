a934507e7d76379cc43832b4cadeb3569e24834e
czy <841265208@qq.com>
2025-12-29 13:47:24 +0800

    chore:文件改名

---
+++ SortAndQuery/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using SortFun.Sort;
+
+namespace SortFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var start = new Start();
+            start.OnStart();
+            
+            Console.WriteLine("work done!");
+        }
+    }
+}
